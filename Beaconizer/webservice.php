c8a8caeecc6a9772a07c2c868611f6a8ea97865b
Ernesto Mendoza Blanco <ernestom@menta.com.mx>
2016-11-24 21:38:26 -0600

    Modifica webservice para incluir título y mensaje.

--- Beaconizer/webservice.php
+++ Beaconizer/webservice.php
@@ -1,14 +1,17 @@
 <?php
 // Correr en modo de desarrollo con el comando: php -S 0.0.0.0:8000
-
 $notifications = array();
+$lipsum = 'Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.';
 for ($i = 1; $i < 10; $i++) {
-  $notifications[] = array('id' => $i, 'message' => 'Este es el aviso con ID ' . $i);
+    $notifications[] = array(
+        'id' => "N$i",
+        'title' => 'Este es el aviso con ID N' . $i,
+        'message' => $lipsum
+    );
 }
 
-// Descomentar una por una al hacer pruebas de app...
-//$notifications[] = array('id' => 10, 'message' => 'Este es el aviso con ID 10');
-//$notifications[] = array('id' => 11, 'message' => 'Este es otro aviso con ID 11');
+//$notifications[] = array('id' => 'N10', 'title' => 'Este es el aviso con ID 10', 'message' => $message);
+//$notifications[] = array('id' => 'N11', 'title' => 'Este es otro aviso con ID 11', 'message' => $message);
 
 $success = true;
 $data = array(
