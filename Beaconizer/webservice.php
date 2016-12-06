055e13746e6e5412363ae175bc7497417ae202c9
Ernesto Mendoza Blanco <ernestom@menta.com.mx>
2016-12-06 01:52:04 -0600

    Actualiza Webservice.

--- Beaconizer/webservice.php
+++ Beaconizer/webservice.php
@@ -1,7 +1,7 @@
 <?php
 // Correr en modo de desarrollo con el comando: php -S 0.0.0.0:8000
 $notifications = array();
-$lipsum = 'Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.';
+$lipsum = 'Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non          proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor   tum poen legum odioque civiuda.';
 for ($i = 1; $i < 10; $i++) {
     $notifications[] = array(
         'id' => "N$i",
@@ -10,8 +10,8 @@ for ($i = 1; $i < 10; $i++) {
     );
 }
 
-//$notifications[] = array('id' => 'N10', 'title' => 'Este es el aviso con ID 10', 'message' => $message);
-//$notifications[] = array('id' => 'N11', 'title' => 'Este es otro aviso con ID 11', 'message' => $message);
+//$notifications[] = array('id' => 'N10', 'title' => 'Este es el aviso con ID 10', 'message' => 'Nueva notificación!!! ' . $lipsum);
+//$notifications[] = array('id' => 'N11', 'title' => 'Este es otro aviso con ID 11', 'message' => 'Otra más!!! ' . $lipsum);
 
 $success = true;
 $data = array(
